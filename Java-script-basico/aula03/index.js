0893f74bf4dce649b7b156ab4e486bdb1c1dcf4a
Matheus alves <106258281+matheusalves07@users.noreply.github.com>
2022-12-31 11:30:30 -0300

    att comentarios e dados

---
+++ Java-script-basico/aula03/index.js
@@ -0,0 +1,12 @@
+console.log('Matheus Alves', 07);
+console.log(`Matheus 07`)
+
+// Exercício aula 03
+
+console.log(`Meu nome é "Matheus Alves." Estou aprendendo Javascript duranta tempo livre nas`, 10, 'manhã e noite')
+
+//utilizar virgula para separa alguns dados
+
+/* 
+
+*/
