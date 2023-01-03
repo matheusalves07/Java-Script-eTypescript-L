26f2fc5aac2ace219be86688c3e22304936727ae
Matheus alves <106258281+matheusalves07@users.noreply.github.com>
2023-01-03 20:54:14 -0300

    Operadores
    Operadores  aritméticos , atribuição , incremento e descremento

---
+++ Java-script-basico/aula11/index.js
@@ -0,0 +1,41 @@
+// OPERADORE ARITMÉTICOS, ATRIBUIÇÃO E INCREMENTO
+
+/*ARITMÉTICO + (adição)-(subtração) /(divisão) *(multiplicação)  **(potenciação)
+% resto da divisão
+*/
+
+/* Precedência do operadores aritiméticos
+
+1 -> ()
+2 -> **
+3 -> * / %
+4 -> + -
+*/
+
+
+//ATRIBUIÇÃO += -= *= /= **=
+
+//INCREMENTO ++ 
+//DECREMENTO --
+
+//let contador = 1 ;
+/*contador ++; // 2
+contador ++; // 3 
+contador ++; // 4
+contador ++; // 5
+console.log(contador)*/
+
+
+//const passo = 50;
+let contador = 2; 
+contador *=2 ; // contador  = contador + passo
+
+console.log(contador);
+
+// NaN - Not a Numb
+
+// parseInt
+//paseFloat
+//Number
+
+
