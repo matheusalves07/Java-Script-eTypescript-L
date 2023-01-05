a12c4022a683747df3feda15e5ac904fc8cc998d
Matheus alves <106258281+matheusalves07@users.noreply.github.com>
2023-01-05 07:07:48 -0300

    Update inde.js

--- Java-script-basico/aula12/js/inde.js
+++ Java-script-basico/aula12/js/inde.js
@@ -1,3 +1,15 @@
 //alert('Olá pessoa')
-window.prompt('Digite seu nome:')
-window.confirm('Deseja realmente apagar?');
+//window.prompt('Digite seu nome:')
+//window.confirm('Deseja realmente apagar?');
+let nome = prompt('Seu nome')
+let num1 = prompt('Digite um número:');
+let num2 = prompt('Digite outro número');
+num1 = Number(num1);
+num2 = Number(num2);
+console.log(typeof num1,typeof num2);
+const resultado = num1 + num2;
+
+alert (`${nome} A soma de ${num1} + ${num2} foi ${resultado}`);
+
+
+//
