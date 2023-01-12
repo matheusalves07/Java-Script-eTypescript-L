efd770de14443853bf65b8fbb160498217cc316b
Matheus alves <106258281+matheusalves07@users.noreply.github.com>
2023-01-12 09:11:24 -0300

    Create aula13.js

---
+++ Java-script-basico/aula14/aula13.js
@@ -0,0 +1,11 @@
+//  indice 012345678 usando  [] dentro do console
+
+let umString = 'Um texto';
+
+console.log( umString);
+console.log(`${umString} em um lindo dia `)
+console.log(umString.indexOf ('texto')) // para descobrir o indice de uma palavra
+console.log(umString.replace(/texto/g,'outra')) //alteração  por outro elemento usando replace
+console.log(umString.length); // tamanho da string
+console.log(umString.slice(2,8))
+
