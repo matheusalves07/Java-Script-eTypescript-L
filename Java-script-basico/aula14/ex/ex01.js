2bfefb2f1b9cec2442368eaff3dd2c17fec95b93
Matheus alves <106258281+matheusalves07@users.noreply.github.com>
2023-01-12 19:23:00 -0300

    incremento com +=

--- Java-script-basico/aula14/ex/ex01.js
+++ Java-script-basico/aula14/ex/ex01.js
@@ -1,1 +1,8 @@
-const nome = prompt ('Digite seu nome');
+const nome = prompt ('Digite seu nome completo');
+
+window.document.body.innerHTML = `Olá ${nome} <br> `;
+
+// += incremente inserções
+
+window.document.body.innerHTML += `O seu nome ${nome}`;
+
