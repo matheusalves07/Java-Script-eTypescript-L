d5b86a29ac1e8ce111cf4cfd5f9f5bc234542a70
Matheus alves <106258281+matheusalves07@users.noreply.github.com>
2023-01-12 21:24:53 -0300

    projeto swim

--- Java-script-basico/aula14/ex/ex01.js
+++ Java-script-basico/aula14/ex/ex01.js
@@ -4,5 +4,21 @@ window.document.body.innerHTML = `Olá ${nome} <br> `;
 
 // += incremente inserções
 
-window.document.body.innerHTML += `O seu nome ${nome}`;
+/*window.document.body.innerHTML += `O seu nome <strong>${nome}</strong><br>`;
+
+window.document.body.innerHTML += `seu nome tem ${nome.length} letras <br>`;
+
+window.document.body.innerHTML += `a seunda letra do seu nome é ${nome[1]} <br>`;
+
+window.document.body.innerHTML += `qual o primeiro índice da letra ${} no seu nome ? <br>`;
+
+window.document.body.innerHTML += `qual o últiimo indice d letra ${} no seu nome ? <br>`;
+
+window.document.body.innerHTML += `as últimas 3 letras do seu nome são  <br>`;
+
+window.document.body.innerHTML += `as palavras do seu nome são  <br>`;
+
+window.document.body.innerHTML += `seu nome com letras maiúsculas  <br>`;
+
+window.document.body.innerHTML += `seu nome com letras menúsculas <br>`;
 
