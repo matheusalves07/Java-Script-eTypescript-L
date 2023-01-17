0b76a9077d084511d4baa33c26bad3f8bfab1c52
Matheus alves <106258281+matheusalves07@users.noreply.github.com>
2023-01-17 10:51:44 -0300

    Math.

---
+++ Java-script-basico/aula16/index.js
@@ -0,0 +1,21 @@
+let num1 = 144;
+
+console.log(num1 ** 0.5) // raiz quadrada
+
+console.log(100/0); //infinity
+
+
+
+//let num2 = Math.floor(num1); arrendonda para baixo
+//let num2 = Math.ceil(num1); arrendonda para acima
+//let num2 = Math.round(num1); //arrendonda para o mais próximo
+
+
+
+//console.log(Math.min(1,2,3,4,5,6,7,-10,-50,1500,));
+//const aleatorio = Math.random() * (10 -5) + (5);// número aleatorio
+console.log(Math.pow(2, 10));
+console.log( 2 **10);
+
+
+ 
