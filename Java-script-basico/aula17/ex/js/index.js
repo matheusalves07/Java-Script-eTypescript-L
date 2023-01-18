fea699396a82a3ef0b8d3adbdd37ba01cf71a562
Matheus alves <106258281+matheusalves07@users.noreply.github.com>
2023-01-18 05:27:34 -0300

    nath e number

---
+++ Java-script-basico/aula17/ex/js/index.js
@@ -0,0 +1,24 @@
+const numero = Number(prompt ('Digite um número'));
+//numero = Number(numero); // convertendo um número
+const numeroTitulo = window.document.getElementById('numero-titulo');
+const texto = window.document.getElementById('texto');
+/*
+Raiz quadrada:
+     é inteiro :
+     é NaN: 
+     arredondando para baixo:
+     arredondando para cima:
+     Com duas casa decimas: 55.20:
+*/
+
+numeroTitulo.innerHTML =  numero;
+texto.innerHTML = '';
+texto.innerHTML = `<p>Raiz quadra é ${numero ** 0.5}</p><br>`;
+texto.innerHTML += `<p>${numero} é inteiro: ${Number.isInteger(numero)}</p><br>`;
+texto.innerHTML += `<p>${numero} é NaN: ${Number.isNaN(numero)}</p><br>`;
+texto.innerHTML += `<p>${numero} é Arrendondando para baixo: ${Math.floor(numero)}</p><br>`;
+texto.innerHTML += `<p>${numero} é Arrendondando para cima: ${Math.ceil(numero)}</p><br>`;
+texto.innerHTML += `<p>${numero} éCom duas casa decimas ${numero.toFixed(2)}</p>`;
+
+
+
