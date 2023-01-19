c9abb4fba438a7c2ddc7729dd7d812a308db4174
Matheus alves <106258281+matheusalves07@users.noreply.github.com>
2023-01-19 15:17:12 -0300

    Array - aula e exercício
    Array = lista de dados  ( string, number , bolean e outros dados utilizados)
    
    usando funções
    1 - push = adiciona no fim
    2 - unshift = adiciona no ínicio
    3 - pop = retira
    4 - remove
    5 - Os índices são os elementor dentro do array , diferente de uma string qe são seus dados fragmentados

---
+++ Java-script-basico/aula18/index.js
@@ -0,0 +1,33 @@
+// ARRAY [] = objeto
+
+// utilizado para fazer listas com string, números e outros tipos de dados
+//const nome = 'Luiz'
+//console.log(nome[0]);
+
+// índices        0       1        2
+const alunos = ['Luiz','Matheus','joão'];
+
+console.log(typeof alunos);
+console.log(alunos instanceof Array); // verificando se é um array
+
+
+//alunos.push('luiza');
+//alunos.push('Eduarda');
+//console.log(alunos.slice(0,-2));
+
+
+//delete alunos[1];
+//const removido = alunos.shift(); // POP REMOVE NO FIM E SHIFT REMOVE NO ÍNICIO
+//console.log(removido );
+//alunos.unshift('Luiza'); // função adiciona no íncio do array
+//alunos.push('Alves'); // função que adiciona no fim do array
+//alunos.push('Castro');// função que adiciona no fim do array
+//alunos.push('Sales');// função que adiciona no fim do array
+//alunos[alunos.length] ='Luiza';
+//alunos[alunos.length] ='Fábio';
+//alunos[alunos.length] ='Raimundo';
+//alunos[3]= 'Carlos';
+//alunos [0] = 'Eduardo'; // alterando o array usando indice 0
+
+//console.log(alunos[0]);
+//console.log(alunos[1]);
