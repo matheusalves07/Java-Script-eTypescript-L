0a0f690703db090eb6d5f05cf5d17b75542216db
Matheus alves <106258281+matheusalves07@users.noreply.github.com>
2023-01-20 13:25:12 -0300

    Update index.js

---
+++ Java-script-basico/aula19/index.js
@@ -0,0 +1,30 @@
+// FUNÇÕES
+
+
+
+/*function saudacao (nome){
+ return `Bom dia ${nome}`;
+
+}
+const variavel = saudacao('Luiz');
+console.log(variavel); */
+
+
+// FUNÇÃO PARA SOMAR
+// return sempre é o fim de uma função , nada funciona após p return
+
+
+/*function soma(x=1, y=1){
+    const resultado = x + y ;
+    return resultado;
+    
+}
+
+const resultado = soma(4);
+console.log(resultado);*/
+
+
+
+const raiz = function (){
+
+};
