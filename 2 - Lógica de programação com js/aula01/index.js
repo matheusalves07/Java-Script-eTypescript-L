5db07a63b819274ef5214096fe61a066e2fbbf08
Matheus alves <106258281+matheusalves07@users.noreply.github.com>
2023-01-25 15:44:42 -0300

    Operado de comparação e String em number
    transformando string em número

--- 2 - Lógica de programação com js/aula01/index.js
+++ 2 - Lógica de programação com js/aula01/index.js
@@ -1,2 +1,22 @@
 // Operadores de Comparação
 
+/*
+    >
+    >= 
+    <
+    <=
+    == igualdade (valor) ** não utilizar
+    === igualde estrita (valor e tipo) checa o tipo
+    != diferente (valor)
+    !== diferente estrito (valor e tipo) checa o tipo
+*/
+const num1 = 10 ;
+const num2 = Number('10'); // transformando string em number
+
+
+console.log(typeof(num2))
+
+const comp = num1 === num2;
+
+
+//console.log(comp);
