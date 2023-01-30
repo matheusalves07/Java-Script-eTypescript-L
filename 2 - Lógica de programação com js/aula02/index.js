00b1955199bab4ce2ac42f445c243447daec8b0e
Matheus alves <106258281+matheusalves07@users.noreply.github.com>
2023-01-30 11:14:00 -0300

    Update index.js

---
+++ 2 - Lógica de programação com js/aula02/index.js
@@ -0,0 +1,21 @@
+// OPERADORES LÓGICOS
+/*
+&&  -> and -> E // todas precisam ser verdadeiras
+||  -> or -> ou 
+!  -> Not -> Não // inverte o valor
+
+*/
+//const expressaoAnd= true && true && true && true ;
+//const expressaoOr= false || true || false || false ;
+//const expressaoNot = true ! true ;
+
+const usuario = 'Luis'; // form usuario digitou
+const senha = '123456'; // form usuario digitou
+const vaiLogar = usuario === 'Luis' && senha === '123456';
+console.log(vaiLogar);
+
+console.log(!!!false)
+
+
+//console.log(expressaoOr);
+//console.log(expressaoNot);
