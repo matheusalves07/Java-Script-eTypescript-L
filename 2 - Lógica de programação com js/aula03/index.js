db5ea8789f09442dcc0d6e76f266bf0a4928b7ed
Matheus alves <106258281+matheusalves07@users.noreply.github.com>
2023-01-31 20:56:09 -0300

    curto circuito

---
+++ 2 - Lógica de programação com js/aula03/index.js
@@ -0,0 +1,24 @@
+// Avaliação de curto circuito
+
+/*
+FALSY -
+false
+0
+' "" ``
+null/undefined
+Na
+*/
+
+/*function falaOi (){
+    return 'Oi';
+
+}
+const vaiExecutar = true;
+console.log( vaiExecutar && falaOi());*/
+
+
+// our || 
+
+/* somente precisa de uma verdadeira */
+
+
