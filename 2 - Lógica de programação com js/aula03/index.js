4cfe3eb038547e4e862e09e012db5f718aac3154
Matheus alves <106258281+matheusalves07@users.noreply.github.com>
2023-02-01 05:42:25 -0300

    Update index.js

--- 2 - Lógica de programação com js/aula03/index.js
+++ 2 - Lógica de programação com js/aula03/index.js
@@ -22,3 +22,21 @@ console.log( vaiExecutar && falaOi());*/
 /* somente precisa de uma verdadeira */
 
 
+//console.log(0 || false || null || 'Mateus'|| true)
+
+/*const corUsuario ='vermelho';
+const corPadrao = corUsuario || 'preto';
+console.log(corPadrao);*/
+
+const a = 0;
+const b = null;
+const c = 'false'; // verdadeiro 
+const d = false;
+const e = NaN;
+
+console.log(a||b||c||d||e);
+
+
+
+
+
