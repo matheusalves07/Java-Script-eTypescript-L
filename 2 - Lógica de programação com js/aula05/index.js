08f4c3c7828fb11b8efbb954fdad4db28cff5ab3
Matheus alves <106258281+matheusalves07@users.noreply.github.com>
2023-02-03 11:34:20 -0300

    Create index.js

---
+++ 2 - Lógica de programação com js/aula05/index.js
@@ -0,0 +1,21 @@
+// continuação if , else if e else
+
+// se e senão 
+
+
+const numero = 15 ;
+
+ if (1 ===1 ){
+    console.log('verdadeiro')
+}
+
+if (numero >= 0 && numero <= 5){
+    console.log("O número está entre 0 e 5")
+
+} 
+ else if(numero > 5 && numero < 20){
+    console.log('numero é maior que 5 e menor que 20');
+} else {
+    console.log( 'chegou')
+}
+
