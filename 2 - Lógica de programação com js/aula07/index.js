5fa3c28d2701d14aa459d668613d41dbf2f98620
Matheus alves <106258281+matheusalves07@users.noreply.github.com>
2023-02-05 07:28:36 -0300

    operação ternário ? ;
    ? usar valores verdadeiros de resposta :
    valores falsos após os (:)

--- 2 - Lógica de programação com js/aula07/index.js
+++ 2 - Lógica de programação com js/aula07/index.js
@@ -1,2 +1,22 @@
 // Operadores ternarios ? :
-// ? :
+// (condição)? 'valor verdadeiro' : 'valor falso '
+
+const pontuacaoUsuario = 800;
+const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuario Vip': 'Usuario normal';
+
+
+const corUsuario = 'azul';
+const corPadrao = corUsuario || 'Preto';
+
+console.log(corPadrao)
+
+console.log(nivelUsuario)
+
+
+
+
+//if (pontuacaoUsuario >=1000){
+//    console.log('você é vip');
+//} else{
+//    console.log('Usuario normal')
+//}
