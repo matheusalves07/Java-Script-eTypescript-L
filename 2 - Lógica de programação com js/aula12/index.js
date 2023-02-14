241ca3e926fc6145ec41170bbee7b80df4811821
Matheus alves <106258281+matheusalves07@users.noreply.github.com>
2023-02-14 09:14:48 -0300

    Update index.js

--- 2 - Lógica de programação com js/aula12/index.js
+++ 2 - Lógica de programação com js/aula12/index.js
@@ -1,2 +1,31 @@
 // Atribuição via desestruturação ( Array)
 
+/*
+let a = 'a';//b
+let b = 'b';//c
+let c = 'c';//a
+
+const letras = [b,c,a];
+[a,b,c] = letras; */
+
+
+
+//... rest , ...spread
+// ...resto  = é o quue sobra do array não definido no console 
+
+/*const numeros = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000];
+
+const [um, , tres, , cinco, , sete] = numeros;
+console.log(um , tres, cinco);*/
+
+
+
+//acessando array dentro de array
+/*const numeros = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+
+console.log(numeros[1][1]);*/
+
+const numeros = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+
+const[,[,, seis]] = numeros;
+console.log(seis);
