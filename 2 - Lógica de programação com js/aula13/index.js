2ba51d5a4d6dc41f5d1c2b7702ba47b4a376b01c
Matheus alves <106258281+matheusalves07@users.noreply.github.com>
2023-02-15 06:10:56 -0300

    Create index.js

---
+++ 2 - Lógica de programação com js/aula13/index.js
@@ -0,0 +1,16 @@
+// Atribuição via desestruturação ( Objetos)
+
+const pessoa = {
+    nome: 'Matheus',
+    sobrenome: 'Alves Castro',
+    idade: 27,
+    endereco: {
+        rua: 'Av. juracy magalhaes',
+        numero: 1161
+    }
+};
+
+const { nome: n = "", sobrenome, idade } = pessoa;
+console.log(n, sobrenome, idade);
+const {endereco: {rua, numero}, endereco} =pessoa;
+console.log(rua, numero, endereco);
