f6aeb32b6d4ec090ba241a5978b825ede6483aed
Matheus alves <106258281+matheusalves07@users.noreply.github.com>
2024-06-10 16:04:27 -0300

    Update ex.js

--- Aula01/ex01.js/ex.js
+++ Aula01/ex01.js/ex.js
@@ -1,4 +1,5 @@
 console.log(' Olá Meu nome é "Matheus". Estou aprendendo JS ás', 10 ,"da manhã");
 console.log("oie");
 console.log("ok");
-console.log("12"); 
+console.log("12 0"); 
+console.log("matheus");
